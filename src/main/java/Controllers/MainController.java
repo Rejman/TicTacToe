7e43533bf92cd43ece9d41b9681e45c61e0baa4e
Rejman <rreejjmmaann@gmail.com>
2019-06-05 20:15:37 +0200

    Refactor

--- src/main/java/Controllers/MainController.java
+++ src/main/java/Controllers/MainController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
@@ -9,55 +10,45 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
 public class MainController {
 
+    private Random generator = new Random();
+    private final int SIZE = 3;
     @FXML
     private GridPane mainGridPane;
-
-    @FXML
-    private ImageView field1ImageView;
-
-    @FXML
-    private ImageView field2ImageView;
-
-    @FXML
-    private ImageView field3ImageView;
-
-    @FXML
-    private ImageView field4ImageView;
-
-    @FXML
-    private ImageView field5ImageView;
-
-    @FXML
-    private ImageView field6ImageView;
-
-    @FXML
-    private ImageView field7ImageView;
-
-    @FXML
-    private ImageView field8ImageView;
-
-    @FXML
-    private ImageView field9ImageView;
 
     private Image cross = new Image("/img/cross.png");
     private Image circle = new Image("/img/circle.png");
     private Image empty = new Image("/img/empty.png");
 
+    private List<Integer> emptyFields;
+    private List<ImageView> fields;
+
     @FXML
     void initialize() {
+        fields = new ArrayList<ImageView>();
+        emptyFields = new ArrayList<Integer>();
+        emptyFields.add(0);
+        emptyFields.add(1);
+        emptyFields.add(2);
+        emptyFields.add(3);
+        emptyFields.add(4);
+        emptyFields.add(5);
+        emptyFields.add(6);
+        emptyFields.add(7);
+        emptyFields.add(8);
 
         buildFields();
-
-
     }
 
     private void buildFields(){
         final int WIDTH = 100;
         final int HEIGHT = 100;
-        final int SIZE = 3;
-
 
         for(int i=0; i<SIZE; i++){
 
@@ -69,11 +60,12 @@ public class MainController {
                 imageView.setImage(empty);
                 imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
                     public void handle(MouseEvent event) {
-                        click(imageView);
+                        int id = mainGridPane.getChildren().indexOf(imageView);
+                        click(id);
                     }
                 });
-
-                mainGridPane.add(imageView, i, j);
+                fields.add(imageView);
+                mainGridPane.add(imageView, j, i);
             }
         }
         mainGridPane.setGridLinesVisible(true);
@@ -81,8 +73,11 @@ public class MainController {
 
     }
 
-    private boolean click(ImageView imageView){
-        if(imageView.getImage()==empty){
+    private boolean click(int id){
+        int field = emptyFields.indexOf(id);
+        if(field>=0){
+            emptyFields.remove(new Integer(id));
+            ImageView imageView = (ImageView) mainGridPane.getChildren().get(id);
             imageView.setImage(cross);
             return true;
         }
@@ -92,4 +87,6 @@ public class MainController {
         }
     }
 
+
 }
+
