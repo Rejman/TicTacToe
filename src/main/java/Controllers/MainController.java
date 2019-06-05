fa64bf6be37e9edd7479c5ca9bdb7f8559842dae
Rejman <rreejjmmaann@gmail.com>
2019-06-05 19:10:05 +0200

    Add game board

--- src/main/java/Controllers/MainController.java
+++ src/main/java/Controllers/MainController.java
@@ -1,209 +1,95 @@
 package Controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 
 public class MainController {
 
+    @FXML
+    private GridPane mainGridPane;
 
     @FXML
-    private VBox mainVBox;
+    private ImageView field1ImageView;
 
     @FXML
-    private ImageView field1;
+    private ImageView field2ImageView;
 
     @FXML
-    private ImageView field2;
+    private ImageView field3ImageView;
 
     @FXML
-    private ImageView field3;
+    private ImageView field4ImageView;
 
     @FXML
-    private ImageView field4;
+    private ImageView field5ImageView;
 
     @FXML
-    private ImageView field5;
+    private ImageView field6ImageView;
 
     @FXML
-    private ImageView field6;
+    private ImageView field7ImageView;
 
     @FXML
-    private ImageView field7;
+    private ImageView field8ImageView;
 
     @FXML
-    private ImageView field8;
+    private ImageView field9ImageView;
 
-    @FXML
-    private ImageView field9;
-
-    private boolean isCross = true;
     private Image cross = new Image("/img/cross.png");
     private Image circle = new Image("/img/circle.png");
-
-
+    private Image empty = new Image("/img/empty.png");
 
     @FXML
     void initialize() {
 
-        Field fields[] = new Field[9];
-        fields[0]=new Field(field1);
-        fields[1]=new Field(field2);
-        fields[2]=new Field(field3);
-        fields[3]=new Field(field4);
-        fields[4]=new Field(field5);
-        fields[5]=new Field(field6);
-        fields[6]=new Field(field7);
-        fields[7]=new Field(field8);
-        fields[8]=new Field(field9);
+        buildFields();
 
-        for (final Field field:fields
-             ) {
-            field.field.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                public void handle(MouseEvent event) {
-                    if(field.isEmpty){
-                        if(isCross){
-                            field.field.setImage(cross);
-                        }
-                        else
-                        {
-                            field.field.setImage(circle);
-                        }
-                        if(isCross) isCross=false;
-                        else isCross=true;
-                        field.isEmpty=false;
+
+    }
+
+    private void buildFields(){
+        final int WIDTH = 100;
+        final int HEIGHT = 100;
+        final int SIZE = 3;
+
+
+        for(int i=0; i<SIZE; i++){
+
+            for(int j=0;j<SIZE; j++){
+
+                final ImageView imageView = new ImageView();
+                imageView.setFitWidth(WIDTH);
+                imageView.setFitWidth(HEIGHT);
+                imageView.setImage(empty);
+                imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    public void handle(MouseEvent event) {
+                        click(imageView);
                     }
-                }
-            });
+                });
+
+                mainGridPane.add(imageView, i, j);
+            }
         }
-
-
-    }
-
-    void click(MouseEvent event){
-
-    }
-
-    @FXML
-    void click1(MouseEvent event) {
+        mainGridPane.setGridLinesVisible(true);
+        mainGridPane.setCursor(Cursor.HAND);
 
     }
 
-    @FXML
-    void click2(MouseEvent event) {
-        if(isCross){
-            field2.setImage(cross);
+    private boolean click(ImageView imageView){
+        if(imageView.getImage()==empty){
+            imageView.setImage(cross);
+            return true;
         }
-        else
-        {
-            field2.setImage(circle);
+        else{
+            System.out.println("Fields is not empty");
+            return false;
         }
-        if(isCross) isCross=false;
-        else isCross=true;
     }
 
-    @FXML
-    void click3(MouseEvent event) {
-        if(isCross){
-            field3.setImage(cross);
-        }
-        else
-        {
-            field3.setImage(circle);
-        }
-        if(isCross) isCross=false;
-        else isCross=true;
-    }
-
-    @FXML
-    void click4(MouseEvent event) {
-        if(isCross){
-            field4.setImage(cross);
-        }
-        else
-        {
-            field4.setImage(circle);
-        }
-        if(isCross) isCross=false;
-        else isCross=true;
-    }
-
-    @FXML
-    void click5(MouseEvent event) {
-        if(isCross){
-            field5.setImage(cross);
-        }
-        else
-        {
-            field5.setImage(circle);
-        }
-        if(isCross) isCross=false;
-        else isCross=true;
-    }
-
-    @FXML
-    void click6(MouseEvent event) {
-        if(isCross){
-            field6.setImage(cross);
-        }
-        else
-        {
-            field6.setImage(circle);
-        }
-        if(isCross) isCross=false;
-        else isCross=true;
-    }
-
-    @FXML
-    void click7(MouseEvent event) {
-        if(isCross){
-            field7.setImage(cross);
-        }
-        else
-        {
-            field7.setImage(circle);
-        }
-        if(isCross) isCross=false;
-        else isCross=true;
-    }
-
-    @FXML
-    void click8(MouseEvent event) {
-        if(isCross){
-            field8.setImage(cross);
-        }
-        else
-        {
-            field8.setImage(circle);
-        }
-        if(isCross) isCross=false;
-        else isCross=true;
-    }
-
-    @FXML
-    void click9(MouseEvent event) {
-        if(isCross){
-            field9.setImage(cross);
-        }
-        else
-        {
-            field9.setImage(circle);
-        }
-        if(isCross) isCross=false;
-        else isCross=true;
-    }
-    class Field{
-        public ImageView field;
-        public boolean isEmpty = true;
-
-        public Field(ImageView field) {
-            this.field = field;
-        }
-    }
 }
