00ebd5e4bbcf82667383184eb3d6ad11e9c107b8
Rejman <rreejjmmaann@gmail.com>
2019-06-25 16:26:26 +0200

    Change game type

--- src/main/java/Controllers/GamePanelController.java
+++ src/main/java/Controllers/GamePanelController.java
@@ -3,7 +3,9 @@ package Controllers;
 import Models.Game.Game;
 import Models.Game.Sign;
 import Models.Gui.GameBoard;
+import Models.Gui.HumanVsComputer;
 import Models.Gui.HumanVsHuman;
+import Models.Player.Computer;
 import Models.Player.Human;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -24,12 +26,12 @@ public class GamePanelController {
         start();
     }
     void start() {
-        int size = 15;
-        int full = 5;
+        int size = 5;
+        int full = 3;
         Game game = new Game(size,full);
         Human one = new Human("one", Sign.CROSS,game);
-        Human two = new Human("two", Sign.CIRCLE,game);
-        HumanVsHuman gameBoard = new HumanVsHuman(game,one,two);
+        Computer two = new Computer("two", Sign.CIRCLE,game);
+        GameBoard gameBoard = new HumanVsComputer(game,one,two,true);
 
         borderStackPane.getChildren().clear();
         borderStackPane.getChildren().add(gameBoard);
